393a294b4b81199ad0fc45a5f9500d2870fa199a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-08-25 17:51:37 +0200

    Update netty-all, netty-codec-http to 4.1.67.Final (#111)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   lazy val Examples = config("example") extend Compile
 
   val slf4jVersion = "1.7.32"
-  val nettyVersion = "4.1.66.Final"
+  val nettyVersion = "4.1.67.Final"
 
   val nettyAll    = "io.netty"      % "netty-all"        % nettyVersion % Compile
   val nettyHttp   = "io.netty"      % "netty-codec-http" % nettyVersion % Compile
