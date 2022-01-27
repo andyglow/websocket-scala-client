042408741e07c3eb7b2203395790c046bf381772
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-01-27 21:03:52 +0100

    Update slf4j-api, slf4j-simple to 1.7.35 (#140)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   lazy val Examples = config("example") extend Compile
 
-  val slf4jVersion = "1.7.33"
+  val slf4jVersion = "1.7.35"
   val nettyVersion = "4.1.73.Final"
 
   val nettyAll    = "io.netty"      % "netty-all"        % nettyVersion % Compile
