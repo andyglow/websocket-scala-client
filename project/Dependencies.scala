ebe7197feb30a89ff49fc3a5206f3702c18ea0fc
Scala Steward <scala_steward@virtuslab.com>
2024-01-16 13:49:29 +0000

    Update netty-all, netty-codec-http to 4.1.105.Final

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   lazy val Examples = config("example") extend Compile
 
   val slf4jVersion       = "2.0.11"
-  val nettyVersion       = "4.1.104.Final"
+  val nettyVersion       = "4.1.105.Final"
   val scalaStmVersion    = "0.11.1"
   val bcVersion          = "1.76" // bouncycastle
   val pekkoHttpVersion   = "1.0.0"
