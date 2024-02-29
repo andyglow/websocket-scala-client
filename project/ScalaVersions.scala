437488ccc1d3e63cf96d8468a0132975e05f2f67
Scala Steward <scala_steward@virtuslab.com>
2024-02-29 14:22:21 +0000

    Update scala-library to 2.13.13

--- project/ScalaVersions.scala
+++ project/ScalaVersions.scala
@@ -1,7 +1,7 @@
 object ScalaVersions {
   val scala211 = "2.11.12"
   val scala212 = "2.12.19"
-  val scala213 = "2.13.12"
+  val scala213 = "2.13.13"
   val scala3   = "3.3.1"
 
   def allScalaVersions(excluding: String => Boolean = _ => false): List[String] =
