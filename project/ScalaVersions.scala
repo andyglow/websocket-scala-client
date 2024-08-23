22c1fc1b5333e6a89514b79628d6ac4f50140806
Scala Steward <scala_steward@virtuslab.com>
2024-08-23 12:48:32 +0000

    Update scala3-library to 3.5.0

--- project/ScalaVersions.scala
+++ project/ScalaVersions.scala
@@ -2,7 +2,7 @@ object ScalaVersions {
   val scala211 = "2.11.12"
   val scala212 = "2.12.19"
   val scala213 = "2.13.14"
-  val scala3   = "3.4.3"
+  val scala3   = "3.5.0"
 
   def allScalaVersions(excluding: String => Boolean = _ => false): List[String] =
     List(scala211, scala212, scala213, scala3).filterNot(excluding)
