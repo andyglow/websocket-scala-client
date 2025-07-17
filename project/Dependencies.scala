afc53e5c69048e3a0d8e7ffedf6b8a7d063e96c2
Scala Steward <scala_steward@virtuslab.com>
2025-07-17 13:38:25 +0000

    Update pekko-stream to 1.1.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val scalaStmVersion    = "0.11.1"
   val bcVersion          = "1.76" // bouncycastle
   val pekkoHttpVersion   = "1.2.0"
-  val pekkoStreamVersion = "1.1.4"
+  val pekkoStreamVersion = "1.1.5"
   // log4j is only needed to test-we-server
   val log4jVersion = "2.21.0"
 
