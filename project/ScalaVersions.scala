cbfa4fd3ceedd454ff4b8e92248a651087aeb38e
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-09-11 15:55:29 +0200

    Update scala3-library to 3.7.3 (#436)
    * Update scala3-library to 3.7.3
    
    * Regenerate GitHub Actions workflow
    
    Executed command: sbt githubWorkflowGenerate

--- project/ScalaVersions.scala
+++ project/ScalaVersions.scala
@@ -2,7 +2,7 @@ object ScalaVersions {
   val scala211 = "2.11.12"
   val scala212 = "2.12.20"
   val scala213 = "2.13.16"
-  val scala3   = "3.7.2"
+  val scala3   = "3.7.3"
 
   def allScalaVersions(excluding: String => Boolean = _ => false): List[String] =
     List(scala211, scala212, scala213, scala3).filterNot(excluding)
